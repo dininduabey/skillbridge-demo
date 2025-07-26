f2c22dc10577c2beba5ee2525abe6b5bd54244d9
Dinindu Abeysuriya <dininduabey@gmail.com>
2025-07-26 17:08:00 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
+import NotFound from "./pages/NotFound";
+import SkillBridgeAuth from "./components/SkillBridgeAuth";
+import ProfileBuilder from "./components/ProfileBuilder";
+import JobSearch from "./components/JobSearch";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<SkillBridgeAuth />} />
+          <Route path="/auth" element={<SkillBridgeAuth />} />
+          <Route path="/profile" element={<ProfileBuilder />} />
+          <Route path="/jobs" element={<JobSearch />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
